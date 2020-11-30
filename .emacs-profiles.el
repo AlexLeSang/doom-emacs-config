3562df3a34109aaca52e91dfec4176b90553190e
Oleksandr Halushko <alexlesang@gmail.com>
2020-11-30 22:01:56 +0100

    Add an example of chemacs profile configuration

---
+++ .emacs-profiles.el
@@ -0,0 +1,5 @@
+(
+ ("default" . ((user-emacs-directory . "~/.emacs.d")))
+ ("clean" . ((user-emacs-directory . "~/.clean.emacs.d")))
+ ("spacemacs" . ((user-emacs-directory . "~/.spacemacs.emacs.d")))
+ )
